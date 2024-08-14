97ff68556382313fad271fdd0e6e1d53946ba64f
vianandi <vianandi60@gmail.com>
2024-08-14 21:38:05 +0700

    create fundwave entity

--- fundwave/entity.go
+++ fundwave/entity.go
@@ -1,1 +1,20 @@
 package fundwave
+
+type Fundwave struct {
+	ID                int    `json:"id"`
+	UserID           int    `json:"userid"`
+	Name              string `json:"name"`
+	Short_description string `json:"short_description"`
+	Perks             string `json:"perks"`
+	Becker_count      int    `json:"becker_count"`
+	Goal_amount       int    `json:"goal_amount"`
+	Current_amount    int    `json:"current_amount"`
+	Slug              string `json:"slug"`
+}
+
+type FundawaveImages struct {
+	ID        int    `json:"id"`
+	FundwaveID int    `json:"fundwave_id"`
+	Filename  string `json:"filename"`
+	isPrimary int	`json:"is_primary"`
+}
