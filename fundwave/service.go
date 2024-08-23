75f24c60cb45ac418357d7d79f0da98fc55a35ce
vianandi <vianandi60@gmail.com>
2024-08-23 20:56:00 +0700

    add: ServiceFundwave

--- fundwave/service.go
+++ fundwave/service.go
@@ -1,1 +1,29 @@
 package fundwave
+
+type Service interface {
+	Findfundawave(userID int) ([]Fundwave, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) Findfundawave(userID int) ([]Fundwave, error) {
+	if userID != 0 {
+		fundwave, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return fundwave, err
+		}
+		return fundwave, nil
+	}
+
+	fundwave, err := s.repository.FindAll()
+	if err != nil {
+		return fundwave, err
+	}
+	return fundwave, nil
+}
